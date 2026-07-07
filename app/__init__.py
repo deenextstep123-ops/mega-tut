e3a3147f52193669ae149a16a0c0657239b395a5
Dee <Deenextstep123@gmail.com>
2026-07-07 18:29:49 +0100

    database start

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
+
+from app.models import User
+
+u = User(username='dimzhang', email='d@gmail.com')
+
+print(u,u.id,u.username,u.email,u.password_hash)
